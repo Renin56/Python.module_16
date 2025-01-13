22157db14598869925ea647f883e442b56a86d9b
Renin56 <kse.renin@yandex.ru>
2025-01-13 07:59:53 +0500

    Update module_16_4.py

--- module_16_4_H/module_16_4.py
+++ module_16_4_H/module_16_4.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path
 from pydantic import BaseModel
 from typing import List
 import uvicorn
+from typing_extensions import Annotated
 
 
 app = FastAPI()
@@ -14,34 +15,44 @@ class User(BaseModel):
     username: str
     age: int
 
+
 @app.get('/users')
 async def get_users() -> List[User]:
     return users
 
 
 @app.post('/user/{username}/{age}')
-async def add_user(username: str, age: int) -> User:
-    user_id = max((u.id for u in users), default=0) + 1
+async def add_user(username: Annotated[str, Path(min_length=3, max_length=20, description='Введите имя')],
+                   age: Annotated[int, Path(ge=18, le=100, description='Укажите возраст')]) -> User:
+
+    user_id = max((user.id for user in users), default=0) + 1
     new_user = User(id=user_id, username=username, age=age)
     users.append(new_user)
+
     return new_user
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int, username: str, age: int) -> User:
+async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Введите ID пользователя от 1 до 100')],
+                      username: Annotated[str, Path(min_length=3, max_length=20, description='Введите имя пользователя')],
+                      age: Annotated[int, Path(ge=18, le=70, description='Укажите возраст пользователя')]) -> User:
+
     for user in users:
         if user.id == user_id:
             user.username = username
             user.age = age
             return user
+
     raise HTTPException(status_code=404, detail='User was not found')
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: int) -> User:
-    for i, u in enumerate(users):
-        if u.id == user_id:
+async def delete_user(user_id: Annotated[int, Path(ge=1, le=100, description='Введите ID пользователя от 1 до 100')]) -> User:
+
+    for i, user in enumerate(users):
+        if user.id == user_id:
             return users.pop(i)
+
     raise HTTPException(status_code=404, detail="User was not found")
 
 
